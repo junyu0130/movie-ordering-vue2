3ff899304a2c15e505dcc7f87d2d9279e67a4098
JunYu <tony.rex.tony.rex01305104@gmail.com>
2022-06-07 17:03:29 +0800

    購物車 縮圖移動動畫

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var vm = new Vue({
   el: "#app",
   data: {
+    isCartOpen: false,
     movies: [],
     cart: [],
+    currentMovie: null,
   },
   // 取得電影資料
   created() {
@@ -24,8 +26,42 @@ var vm = new Vue({
         left: "+=" + -evt.deltaY * 3 + "px",
       });
     },
-    addCart(movie) {
-      this.cart.push(movie);
+    addCart(movie, evt) {
+      this.currentMovie = movie;
+      let target = evt.target;
+      this.$nextTick(() => {
+        TweenMax.fromTo(
+          ".buyBox",
+          1,
+          {
+            opacity: 1,
+            left: $(target).offset().left,
+            top: $(target).offset().top,
+          },
+          {
+            opacity: 0,
+            left: $(".fixed-control").offset().left,
+            top: $(".fixed-control").offset().top,
+          }
+        );
+      });
+
+      setTimeout(() => {
+        this.cart.push(movie);
+      }, 1000);
+    },
+  },
+  watch: {
+    // 觀察購物車是否有加入點選的物件 cart size 變化
+    cart() {
+      TweenMax.from(".fa-cart-shopping", 0.3, { scale: 0.5 });
+    },
+  },
+  computed: {
+    totalPrice() {
+      return this.cart
+        .map((movie) => movie.price)
+        .reduce((total, p) => total + p, 0);
     },
   },
 });
